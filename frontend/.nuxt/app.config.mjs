fee98ce68116ad16928774f23debcca8e005cd88
Nick Moore <nmoore@rushmarket.com>
2023-03-05 00:42:59 -0500

    Adding the nuxt config folder just incase the move is worth it

---
+++ frontend/.nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
