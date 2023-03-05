01bc377aac2b2bb7eff28c57fd8b5c606c450cb0
Nick Moore <nmoore@rushmarket.com>
2023-03-05 01:11:30 -0500

    Adding the base controller and bootstrap

---
+++ backend/www/api/index.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__."/bootstrap.php";
+
+// Load the API controllers
+require PROJECT_ROOT_PATH . "/controller/";
+require PROJECT_ROOT_PATH . "/controller/";
+
+// Initialize API controllers
+?>
