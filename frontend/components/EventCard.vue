7097152946e21617ed235f2bfc14afee13bb7270
Nick Moore <nmoore@rushmarket.com>
2023-03-09 23:45:24 -0500

    feature -> Building out the event cards for the home page

---
+++ frontend/components/EventCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex col justify-start items-center nowrap p-1 event-card">
+    <div class="card-header">
+      <h1 class="title card-title">{{ title }}</h1>
+    </div>
+    <div class="card-content">
+      <p class="body-info">{{ startTime }} - {{ endTime }}</p>
+      <h2 class="title-info">{{ location }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  startTime: String,
+  endTime: String,
+  title: String,
+  location: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.event-card {
+  background-color: $off-white;
+  min-height: 20rem;
+  min-width: 15rem;
+  max-width: 20rem;
+  border-radius: 0.25rem;
+}
+
+.card-header {
+  width: 100%;
+  border-bottom: solid $uc-red 4px;
+  border-radius: .15rem;
+}
+
+.card-title {
+  text-align: center;
+  font-size: 1.75rem;
+}
+
+.body-info,
+.title-info {
+  width: 100%;
+  text-align: center;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,10 +3,57 @@
     <Hero />
     <main class="w-full flex col justify-center items-center my-1 px-2">
       <ForumCard />
+      <div class="flex row wrap justify-center items-center w-full my-1 p-1 event-cards">
+        <EventCard
+          v-for="event in Events"
+          :startTime=event.startTime
+          :endTime=event.endTime
+          :title=event.title
+          :location=event.location
+        />
+      </div>
     </main>
   </div>
 </template>
 
-<style lang="scss">
+<script setup lang="ts">
+  const Events = [
+    {
+      startTime: "8:00",
+      endTime: "9:15",
+      title: "Registration",
+      location: "Hutton School of Business"
+    },
+    {
+      startTime: "9:30",
+      endTime: "10:15",
+      title: "Keynote/Opening Session",
+      location: "Kohn Theatre"
+    },   
+    {
+      startTime: "10:30",
+      endTime: "11:15",
+      title: "Session 1",
+      location: "Hutton School of Business"
+    },
+    {
+      startTime: "11:30",
+      endTime: "12:15",
+      title: "Session 2",
+      location: "Hutton School of Business"
+    },
+    {
+      startTime: "12:15",
+      endTime: "12:30",
+      title: "Additional Meet & Greet",
+      location: "Hutton School of Business"
+    },
 
+  ]
+</script>
+
+<style lang="scss" scoped>
+.event-cards {
+  gap: .5rem;
+}
 </style>
