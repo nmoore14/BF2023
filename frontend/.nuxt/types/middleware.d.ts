2eba69b79a3a4d9ecbe29abd792f7354beeb3980
Nick Moore <nmoore@rushmarket.com>
2023-03-20 16:36:53 -0400

    8-Git setup

--- frontend/.nuxt/types/middleware.d.ts
+++ frontend/.nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = string
-declare module "/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_sass@1.58.3/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
