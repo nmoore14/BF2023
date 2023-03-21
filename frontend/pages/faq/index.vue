f04e4c8aebbd7de2f5045b3c33cfb98841a55948
Nick Moore <nmoore@rushmarket.com>
2023-03-20 21:25:36 -0400

    8-FAQ page work

--- frontend/pages/faq/index.vue
+++ frontend/pages/faq/index.vue
@@ -1,11 +1,66 @@
 <template>
   <div class="flex col justify-center items-center w-full h-full page">
     <main class="w-full flex col justify-center items-center my-1 px-2">
-      <h1 class="title">FAQ</h1>
+      <div class="page-title">
+        <h1 class="title">FAQ</h1>
+      </div>
+      <article class="flex col nowrap justify-center items-center w-full">
+        <section class="faq-section">
+          <h2 class="faq-title">
+            Who should attend the Business Forum for Young Professionals?
+          </h2>
+          <div class="flex column wrap justify-center items-center faq-section-content">
+            <p class="body-text">
+              Anyone who is an in-seat student enrolled in an Accounting or
+              Business class during the session times.
+            </p>
+            <ul>
+              <li class="body-text">
+                ALL 9:25 Accounting/Business courses will meet in the Kohn Theatre for the Keynote Session in lieu of
+                class.
+              </li>
+              <li class="body-text">
+                ALL 10:50 Accounting/Business courses will meet in EITHER Session 1 (10:30 – 11:15) or Session 2 (11:30 –
+                12:15) in the Hutton School of Business. Depending on the student schedule, they can choose which session
+                works best for them.
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">
+            Who will be speaking at the Business Forum for Young Professionals?
+          </h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">
+            When and where will the Business Forum for Young Professionals take
+            place?
+          </h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">What attire is acceptable for this event?</h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">Is Registration required to attend?</h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">Do I receive a Registration Packet?</h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">Where are pictures of the event located?</h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">Accessibility Seating</h2>
+        </section>
+        <section class="faq-section">
+          <h2 class="faq-title">
+            Any other questions/concerns can be sent to the Forum Event
+          </h2>
+        </section>
+      </article>
     </main>
   </div>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
