15832e3f69eaca2e1df64102bfbb6f086522d913
Nick Moore <nmoore@rushmarket.com>
2023-03-23 21:53:07 -0400

    Pre merge work

--- frontend/.nuxt/types/nitro.d.ts
+++ frontend/.nuxt/types/nitro.d.ts
@@ -2,11 +2,14 @@
 declare module 'nitropack' {
   type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
   interface InternalApi {
+    '/__nuxt_error': {
+      'default': Awaited<ReturnType<typeof import('../../node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/renderer').default>>
+    }
   }
 }
 declare global {
-  const __buildAssetsURL: typeof import('/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/paths')['buildAssetsURL']
-  const __publicAssetsURL: typeof import('/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/paths')['publicAssetsURL']
+  const __buildAssetsURL: typeof import('/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/paths')['buildAssetsURL']
+  const __publicAssetsURL: typeof import('/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/paths')['publicAssetsURL']
   const appendCorsHeaders: typeof import('h3')['appendCorsHeaders']
   const appendCorsPreflightHeaders: typeof import('h3')['appendCorsPreflightHeaders']
   const appendHeader: typeof import('h3')['appendHeader']
