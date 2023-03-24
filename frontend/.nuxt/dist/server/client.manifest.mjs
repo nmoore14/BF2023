2526d1f5c49df2e0fe8443bdb593dd029412dcd5
Nick Moore <nmoore@rushmarket.com>
2023-03-23 22:27:23 -0400

    Building newest version 3/23

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++ frontend/.nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,350 @@
-export { default } from "file:///Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/node_modules/.pnpm/@nuxt+vite-builder@3.2.2_fuvyhatis2lxjrl2p5zrm4idiu/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export default {
+  "_app.config.5e97037f.js": {
+    "resourceType": "script",
+    "module": true,
+    "file": "app.config.5e97037f.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.dd29d79a.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.52625ec8.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs",
+      "_app.config.5e97037f.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.dd29d79a.css": {
+    "file": "error-404.dd29d79a.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.26873dcc.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.ae77f080.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs",
+      "_app.config.5e97037f.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.26873dcc.css": {
+    "file": "error-500.26873dcc.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.c506563f.css",
+    "src": "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "assets": [
+      "primeicons.ce852338.eot",
+      "primeicons.3824be50.woff2",
+      "primeicons.90a58d3a.woff",
+      "primeicons.131bc3bf.ttf",
+      "primeicons.5e10f102.svg"
+    ],
+    "css": [
+      "entry.c506563f.css"
+    ],
+    "dynamicImports": [
+      "virtual:nuxt:/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/.nuxt/error-component.mjs"
+    ],
+    "file": "entry.ba21040c.js",
+    "isEntry": true,
+    "src": "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+  },
+  "entry.c506563f.css": {
+    "file": "entry.c506563f.css",
+    "resourceType": "style"
+  },
+  "primeicons.ce852338.eot": {
+    "file": "primeicons.ce852338.eot",
+    "resourceType": "font",
+    "mimeType": "font/eot"
+  },
+  "primeicons.3824be50.woff2": {
+    "file": "primeicons.3824be50.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "primeicons.90a58d3a.woff": {
+    "file": "primeicons.90a58d3a.woff",
+    "resourceType": "font",
+    "mimeType": "font/woff"
+  },
+  "primeicons.131bc3bf.ttf": {
+    "file": "primeicons.131bc3bf.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "primeicons.5e10f102.svg": {
+    "file": "primeicons.5e10f102.svg",
+    "resourceType": "image",
+    "mimeType": "image/svg+xml"
+  },
+  "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.eot": {
+    "resourceType": "font",
+    "mimeType": "font/eot",
+    "file": "primeicons.ce852338.eot",
+    "src": "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.eot"
+  },
+  "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.svg": {
+    "resourceType": "image",
+    "mimeType": "image/svg+xml",
+    "file": "primeicons.5e10f102.svg",
+    "src": "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.svg"
+  },
+  "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "primeicons.131bc3bf.ttf",
+    "src": "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.ttf"
+  },
+  "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.woff": {
+    "resourceType": "font",
+    "mimeType": "font/woff",
+    "file": "primeicons.90a58d3a.woff",
+    "src": "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.woff"
+  },
+  "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "primeicons.3824be50.woff2",
+    "src": "node_modules/.pnpm/primeicons@6.0.1/node_modules/primeicons/fonts/primeicons.woff2"
+  },
+  "pages/faq/index.css": {
+    "resourceType": "style",
+    "file": "index.0693361b.css",
+    "src": "pages/faq/index.css"
+  },
+  "pages/faq/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.2b430378.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/faq/index.vue"
+  },
+  "index.0693361b.css": {
+    "file": "index.0693361b.css",
+    "resourceType": "style"
+  },
+  "pages/forum/index.css": {
+    "resourceType": "style",
+    "file": "index.2f084d0c.css",
+    "src": "pages/forum/index.css"
+  },
+  "pages/forum/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.96f631f6.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/forum/index.vue"
+  },
+  "index.2f084d0c.css": {
+    "file": "index.2f084d0c.css",
+    "resourceType": "style"
+  },
+  "pages/index.css": {
+    "resourceType": "style",
+    "file": "index.39ec2e77.css",
+    "src": "pages/index.css"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.fd5ac546.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  },
+  "index.39ec2e77.css": {
+    "file": "index.39ec2e77.css",
+    "resourceType": "style"
+  },
+  "pages/loginRegister/index.css": {
+    "resourceType": "style",
+    "file": "index.ff714f15.css",
+    "src": "pages/loginRegister/index.css"
+  },
+  "pages/loginRegister/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.30767a48.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/loginRegister/index.vue"
+  },
+  "index.ff714f15.css": {
+    "file": "index.ff714f15.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/BarbaraJohnston.css": {
+    "resourceType": "style",
+    "file": "BarbaraJohnston.3fd1faf9.css",
+    "src": "pages/speakers/BarbaraJohnston.css"
+  },
+  "pages/speakers/BarbaraJohnston.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "BarbaraJohnston.bda39b6b.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/BarbaraJohnston.vue"
+  },
+  "BarbaraJohnston.3fd1faf9.css": {
+    "file": "BarbaraJohnston.3fd1faf9.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/CharlesReed.css": {
+    "resourceType": "style",
+    "file": "CharlesReed.98da1fcd.css",
+    "src": "pages/speakers/CharlesReed.css"
+  },
+  "pages/speakers/CharlesReed.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "CharlesReed.4f016a7f.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/CharlesReed.vue"
+  },
+  "CharlesReed.98da1fcd.css": {
+    "file": "CharlesReed.98da1fcd.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/DavidHubbard.css": {
+    "resourceType": "style",
+    "file": "DavidHubbard.e1f4efc7.css",
+    "src": "pages/speakers/DavidHubbard.css"
+  },
+  "pages/speakers/DavidHubbard.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "DavidHubbard.6128230b.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/DavidHubbard.vue"
+  },
+  "DavidHubbard.e1f4efc7.css": {
+    "file": "DavidHubbard.e1f4efc7.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/JeffDavis.css": {
+    "resourceType": "style",
+    "file": "JeffDavis.11b1ad8e.css",
+    "src": "pages/speakers/JeffDavis.css"
+  },
+  "pages/speakers/JeffDavis.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "JeffDavis.524ff40a.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/JeffDavis.vue"
+  },
+  "JeffDavis.11b1ad8e.css": {
+    "file": "JeffDavis.11b1ad8e.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/JoeCraft.css": {
+    "resourceType": "style",
+    "file": "JoeCraft.b694bb0c.css",
+    "src": "pages/speakers/JoeCraft.css"
+  },
+  "pages/speakers/JoeCraft.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "JoeCraft.429ff0fe.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/JoeCraft.vue"
+  },
+  "JoeCraft.b694bb0c.css": {
+    "file": "JoeCraft.b694bb0c.css",
+    "resourceType": "style"
+  },
+  "pages/speakers/index.css": {
+    "resourceType": "style",
+    "file": "index.27a4693e.css",
+    "src": "pages/speakers/index.css"
+  },
+  "pages/speakers/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "index.e58c84a5.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/speakers/index.vue"
+  },
+  "index.27a4693e.css": {
+    "file": "index.27a4693e.css",
+    "resourceType": "style"
+  },
+  "virtual:nuxt:/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "error-component.e1f95610.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "virtual:nuxt:/Users/nickmoore_rush/Desktop/projects/school/bf2023/frontend/.nuxt/error-component.mjs"
+  }
+}
