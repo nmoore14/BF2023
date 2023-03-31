56ee15cb569ed22b9fde51c800948966f6307052
Nick Moore <nmoore@rushmarket.com>
2023-03-31 03:37:35 -0400

    7 - Build testing

--- frontend/.nuxt/types/nitro.d.ts
+++ frontend/.nuxt/types/nitro.d.ts
@@ -2,9 +2,6 @@
 declare module 'nitropack' {
   type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
   interface InternalApi {
-    '/__nuxt_error': {
-      'default': Awaited<ReturnType<typeof import('../../node_modules/.pnpm/nuxt@3.2.2_eslint@8.36.0+sass@1.58.3/node_modules/nuxt/dist/core/runtime/nitro/renderer').default>>
-    }
   }
 }
 declare global {
